2b6780fc48e128424b1dd7f090afcd30cf71cf65
BigT001 <unclet1992@gmail.com>
2023-10-27 10:45:29 -0400

    user file and sign in

--- wlc.c
+++ wlc.c
@@ -5,7 +5,45 @@
 
 void welcome()
 {
-	printf("Welcome to city bank\n\n");
+	printf("Welcome to city bank\n");
 
+	printf("SIGNUP\n");
+	printf("LOGIN\n");
+	printf("Exit");
 }
 
+int main()
+{
+	int validate;
+	char input[10];
+
+	do {
+		welcome();
+		printf("Enter option: ");
+		fgets(input, sizeof(input), stdin);
+		sscanf(input, "%d", &validate);
+
+		if (validate < 1 || validate > 3)
+			printf("Invalid. try agian.\n");
+			continue;
+		}
+
+		switch (validate) {
+			case 1:
+				signup();
+				break;
+			case 2:
+				if (login()) {
+					printf("login successful");
+					/**logic will be added here.*/
+				}
+				break;
+			case 3:
+				printf("Goodbye!\n");
+				exit(0);
+			default:
+				printf("Invalid choice.try again\n");
+				break;
+		}
+return 0;
+}
